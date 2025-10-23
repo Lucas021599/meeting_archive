f19eaef6a8b5e048642bfe1a98620af37f077403
yunjiwon <yjiwon0422@medicalai.com>
2025-10-23 22:43:42 +0900

    feat: vector search api

--- app/serializers/vector.py
+++ app/serializers/vector.py
@@ -3,3 +3,9 @@ from rest_framework import serializers
 
 class VectorInsertRequestSerializer(serializers.Serializer):
     text = serializers.CharField()
+
+
+class VectorSearchResponseSerializer(serializers.Serializer):
+    id = serializers.CharField()
+    score = serializers.FloatField()
+    text = serializers.CharField()
